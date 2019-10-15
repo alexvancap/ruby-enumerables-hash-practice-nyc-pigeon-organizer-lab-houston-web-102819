aa0ffd767f52a02d532b2372b023ca1abc51a63e
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-15 12:42:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,9 +3,9 @@ def nyc_pigeon_organizer(data)
 
   data.each{|key, value|
     value.each{|key1, value1|
-      value1.each{|names|
-        if !new_hash.has_key?(names)
-          new_hash[names] = []
+      value1.each{|name|
+        if !new_hash.has_key?(name)
+          new_hash[name] = []
         end
 
         if !new_hash[name].has_key?(key)
