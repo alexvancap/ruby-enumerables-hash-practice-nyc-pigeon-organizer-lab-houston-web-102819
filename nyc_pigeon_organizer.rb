8cdf5553acd9ab4cdfe86bdb0f5821d5be94cf7f
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-15 12:43:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,21 +1,21 @@
 def nyc_pigeon_organizer(data)
   pigeons = {}
-
-  data.each{|key, value|
-    value.each{|key1, value1|
-      value1.each{|name|
+    data.each do |key, specs|
+      specs.each do |value, names|
+        names.each do |name|
         if not pigeons.has_key?(name)
-       pigeons[name] = {}
-     end
+          pigeons[name] = {}
+        end
 
-     if not pigeons[name].has_key?(key)
-       pigeons[name][key] = []
-     end
+        if not pigeons[name].has_key?(key)
+          pigeons[name][key] = []
+        end
 
-     pigeons[name][key] << value.to_s
-   end
-    }
-   }
+        pigeons[name][key] << value.to_s
+      end
+    end
+  end
+  pigeons
 end
 
 
